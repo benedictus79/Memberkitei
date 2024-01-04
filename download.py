bbdfc143f3622904e1e0b1b5ffcff0baa3ee41a6
viniped <viniciuscolamego@gmail.com>
2024-01-04 09:00:16 -0300

    Added support for ARIA2C as an external downloader.

--- download.py
+++ download.py
@@ -1,4 +1,5 @@
 import yt_dlp
+from time import sleep
 from utils import SilentLogger, log_error
 
 
@@ -7,14 +8,6 @@ pandavideoheaders = lambda rerefer, optional_origin=None: {
   'Referer': rerefer,
   **({'Origin': optional_origin} if optional_origin is not None else {})
 }
-
-
-def verified_retry_download(url, session, ydl_opts):
-  headers = pandavideoheaders(session.url, session.url)
-  ydl_opts['http_headers'] = headers
-  with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([url])
-
 
 def download_video(url, output_name, session):
   headers = pandavideoheaders(session.url)
@@ -32,13 +25,34 @@ def download_video(url, output_name, session):
     'retries': 30,
     'continuedl': True,
     'extractor_retries': 10,
-  }
-
+    'external_downloader': 'aria2c',
+    'external_downloader_args': [
+        '--allow-overwrite=true',
+        '--file-allocation=none',
+        '--console-log-level=error',
+        '--download-result=hide',
+        '--summary-interval=0',
+        '-x16', '-j16', '-s16',
+        '--max-connection-per-server=16',
+        '--min-split-size=10M',
+        '--split=16',
+        '--optimize-concurrent-downloads',
+        '--max-overall-download-limit=0',
+        '--quiet=true'
+          ]
+                }
   try:
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-      ydl.download([url])
+        ydl.download([url])
   except yt_dlp.utils.DownloadError as e:
     if '403' in str(e) or '401' in str(e):
       verified_retry_download(url, session, ydl_opts)
     log_error(f"Erro ao baixar: {e}")
     pass
+
+
+def verified_retry_download(url, session, ydl_opts):
+  headers = pandavideoheaders(session.url, session.url)
+  ydl_opts['http_headers'] = headers
+  with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+    ydl.download([url])
